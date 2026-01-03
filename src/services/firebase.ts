f158bea127050ddea5bd75f6fcd609c87f441e15
IlhamHattaManggala <ilhamhattamanggala31@gmail.com>
2026-01-03 08:57:22 +0700

    Update pake .env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, runTransaction, onValue, DataSnapshot } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDk_oRk8CIwxZTOP90CtQkiDnzG4TEG1_c",
-  authDomain: "portofolio-5690a.firebaseapp.com",
-  projectId: "portofolio-5690a",
-  storageBucket: "portofolio-5690a.firebasestorage.app",
-  messagingSenderId: "576686727272",
-  appId: "1:576686727272:web:10acedbf904c4ebbc19bdd",
-  measurementId: "G-EPESDR0B43"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
